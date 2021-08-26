5c3f6cd5e8a292a91573e26356ac167694ad52e4
Aaron Holtzman <aaron@appliedmind.ca>
2021-08-25 22:57:43 -0700

    add UBX-CFG-NAVX5 packet

--- ublox/src/ubx_packets/packets.rs
+++ ublox/src/ubx_packets/packets.rs
@@ -1028,6 +1028,120 @@ impl<T: FloatCore + FromPrimitive + ToPrimitive> ScaleBack<T> {
     }
 }
 
+/// Navigation Engine Expert Settings
+#[ubx_packet_recv_send]
+#[ubx(
+    class = 0x06,
+    id = 0x23,
+    fixed_payload_len = 44,
+    flags = "default_for_builder"
+)]
+struct CfgNavX5 {
+    /// Only version 3 supported
+    version: u16,
+
+    /// Only the masked parameters will be applied
+    #[ubx(map_type = CfgNavX5Params1)]
+    mask1: u16,
+
+    #[ubx(map_type = CfgNavX5Params2)]
+    mask2: u32,
+
+    /// Reserved
+    reserved1: [u8; 2],
+
+    /// Minimum number of satellites for navigation
+    min_svs: u8,
+
+    ///Maximum number of satellites for navigation
+    max_svs: u8,
+
+    /// Minimum satellite signal level for navigation
+    min_cno_dbhz: u8,
+
+    /// Reserved
+    reserved2: u8,
+
+    /// initial fix must be 3D
+    ini_fix_3d: u8,
+
+    /// Reserved
+    reserved3: [u8; 2],
+
+    /// issue acknowledgements for assistance message input
+    ack_aiding: u8,
+
+    /// GPS week rollover number
+    wkn_rollover: u16,
+
+    /// Permanently attenuated signal compensation
+    sig_atten_comp_mode: u8,
+
+    /// Reserved
+    reserved4: u8,
+    reserved5: [u8; 2],
+    reserved6: [u8; 2],
+
+    /// Use Precise Point Positioning (only available with the PPP product variant)
+    use_ppp: u8,
+
+    /// AssistNow Autonomous configuration
+    aop_cfg: u8,
+
+    /// Reserved
+    reserved7: [u8; 2],
+
+    /// Maximum acceptable (modeled) AssistNow Autonomous orbit error
+    aop_orb_max_err: u16,
+
+    /// Reserved
+    reserved8: [u8; 4],
+    reserved9: [u8; 3],
+
+    /// Enable/disable ADR/UDR sensor fusion 
+    use_adr: u8,
+
+    /// Reserved
+    reserved10: [u8; 2],
+    reserved11: [u8; 2],
+}
+
+#[ubx_extend_bitflags]
+#[ubx(from, into_raw, rest_reserved)]
+bitflags! {
+    /// `CfgNavX51` parameters bitmask
+    #[derive(Default)]
+    pub struct CfgNavX5Params1: u16 {
+        /// apply min/max SVs settings
+        const MIN_MAX = 0x4;
+        /// apply minimum C/N0 setting
+        const MIN_CNO = 0x8;
+        /// apply initial 3D fix settings
+        const INITIAL_3D_FIX = 0x40;
+        /// apply GPS weeknumber rollover settings
+        const WKN_ROLL = 0x200;
+        /// apply assistance acknowledgement settings
+        const AID_ACK = 0x400;
+        /// apply usePPP flag
+        const USE_PPP = 0x2000;
+        /// apply aopCfg (useAOP flag) and aopOrbMaxErr settings (AssistNow Autonomous)
+        const AOP_CFG = 0x4000;
+    }
+}
+
+#[ubx_extend_bitflags]
+#[ubx(from, into_raw, rest_reserved)]
+bitflags! {
+    /// `CfgNavX5Params2` parameters bitmask
+    #[derive(Default)]
+    pub struct CfgNavX5Params2: u32 {
+        ///  apply ADR/UDR sensor fusion on/off setting
+        const USE_ADR = 0x40;
+        ///  apply signal attenuation compensation feature settings
+        const USE_SIG_ATTEN_COMP = 0x80;
+    }
+}
+
 /// Hardware status
 #[ubx_packet_recv]
 #[ubx(class = 0x0a, id = 0x09, fixed_payload_len = 60)]
