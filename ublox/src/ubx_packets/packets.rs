4cc794ac4d123237c6fe658eaf3b912affe6701a
Aaron Holtzman <aaron@appliedmind.ca>
2021-08-30 08:09:03 -0700

    Add UBX-INF packets

--- ublox/src/ubx_packets/packets.rs
+++ ublox/src/ubx_packets/packets.rs
@@ -441,6 +441,98 @@ bitflags! {
     }
 }
 
+#[ubx_packet_recv]
+#[ubx(
+    class = 0x4,
+    id = 0x0,
+    max_payload_len = 1240,
+    flags = "default_for_builder"
+)]
+struct InfError{
+    #[ubx(map_type = &str, 
+        may_fail, 
+        is_valid = inf::is_valid,
+        from = inf::convert_to_str,
+        get_as_ref)]
+    message: [u8; 0]
+}
+
+#[ubx_packet_recv]
+#[ubx(
+    class = 0x4,
+    id = 0x2,
+    max_payload_len = 1240,
+    flags = "default_for_builder"
+)]
+struct InfNotice{
+    #[ubx(map_type = &str, 
+        may_fail, 
+        is_valid = inf::is_valid,
+        from = inf::convert_to_str,
+        get_as_ref)]
+    message: [u8; 0]
+}
+
+#[ubx_packet_recv]
+#[ubx(
+    class = 0x4,
+    id = 0x3,
+    max_payload_len = 1240,
+    flags = "default_for_builder"
+)]
+struct InfTest{
+    #[ubx(map_type = &str, 
+        may_fail, 
+        is_valid = inf::is_valid,
+        from = inf::convert_to_str,
+        get_as_ref)]
+    message: [u8; 0]
+}
+
+#[ubx_packet_recv]
+#[ubx(
+    class = 0x4,
+    id = 0x1,
+    max_payload_len = 1240,
+    flags = "default_for_builder"
+)]
+struct InfWarning{
+    #[ubx(map_type = &str, 
+        may_fail, 
+        is_valid = inf::is_valid,
+        from = inf::convert_to_str,
+        get_as_ref)]
+    message: [u8; 0]
+}
+
+
+#[ubx_packet_recv]
+#[ubx(
+    class = 0x4,
+    id = 0x4,
+    max_payload_len = 1240,
+    flags = "default_for_builder"
+)]
+struct InfDebug{
+    #[ubx(map_type = &str, 
+        may_fail, 
+        is_valid = inf::is_valid,
+        from = inf::convert_to_str,
+        get_as_ref)]
+    message: [u8; 0]
+}
+
+
+mod inf {
+    pub(crate) fn convert_to_str(bytes: &[u8]) -> &str {
+        core::str::from_utf8(bytes).unwrap()
+    }
+
+    pub(crate) fn is_valid(bytes: &[u8]) -> bool {
+        true
+    }
+}
+
 #[ubx_packet_send]
 #[ubx(
     class = 0x0B,
@@ -1228,7 +1320,6 @@ pub enum AntennaPower {
     DontKnow = 2,
 }
 
-
 /// Receiver/Software Version
 #[ubx_packet_recv]
 #[ubx(class = 0x0a, id = 0x04, max_payload_len = 1240)]
@@ -1302,6 +1393,11 @@ define_recv_packets!(
         CfgPrtUart,
         CfgNav5,
         CfgAnt,
+        InfError,
+        InfWarning,
+        InfNotice,
+        InfTest,
+        InfDebug,
         MonVer,
         MonHw
     }
