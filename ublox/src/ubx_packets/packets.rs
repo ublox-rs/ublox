48fa443aabf2b24c8d25d75a9c38136c7f64cdbc
Matt Ickstadt <mattico8@gmail.com>
2022-03-15 11:14:44 -0500

    Add Odometer packets

--- ublox/src/ubx_packets/packets.rs
+++ ublox/src/ubx_packets/packets.rs
@@ -638,6 +638,78 @@ mod navsat {
     }
 }
 
+/// Odometer solution
+#[ubx_packet_recv]
+#[ubx(class = 0x01, id = 0x09, fixed_payload_len = 20)]
+struct NavOdo {
+    version: u8,
+    reserved: [u8; 3],
+    i_tow: u32,
+    distance: u32,
+    total_distance: u32,
+    distance_std: u32,
+}
+
+/// Reset odometer
+#[ubx_packet_send]
+#[ubx(class = 0x01, id = 0x10, fixed_payload_len = 0)]
+struct NavResetOdo { }
+
+/// Configure odometer
+#[ubx_packet_recv_send]
+#[ubx(class = 0x06, id = 0x1E, fixed_payload_len = 20, flags = "default_for_builder")]
+struct CfgOdo {
+    version: u8,
+    reserved: [u8; 3],
+    /// Odometer COG filter flags. See [OdoCogFilterFlags] for details.
+    #[ubx(map_type = OdoCogFilterFlags)]
+    flags: u8,
+    #[ubx(map_type = OdoProfile, may_fail)]
+    odo_cfg: u8,
+    reserved2: [u8; 6],
+    cog_max_speed: u8,
+    cog_max_pos_acc: u8,
+    reserved3: [u8; 2],
+    vel_lp_gain: u8,
+    cog_lp_gain: u8,
+    reserved4: [u8; 2],
+}
+
+#[ubx_extend_bitflags]
+#[ubx(from, into_raw, rest_reserved)]
+bitflags! {
+    #[derive(Default)]
+    pub struct OdoCogFilterFlags: u8 {
+        /// Odometer enabled flag
+        const USE_ODO = 0x01;
+        /// Low-speed COG filter enabled flag
+        const USE_COG = 0x02;
+        /// Output low-pass filtered velocity flag
+        const OUT_LP_VEL = 0x04;
+        /// Output low-pass filtered heading (COG) flag
+        const OUT_LP_COG = 0x08;
+    }
+}
+
+/// Odometer configuration profile
+#[ubx_extend]
+#[ubx(from_unchecked, into_raw, rest_error)]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug)]
+pub enum OdoProfile {
+    Running = 0,
+    Cycling = 1,
+    Swimming = 2,
+    Car = 3,
+    Custom = 4,
+}
+
+impl Default for OdoProfile {
+    fn default() -> Self {
+        Self::Running
+    }
+}
+
 /// Information message conifg
 #[ubx_packet_recv_send]
 #[ubx(
@@ -646,7 +718,7 @@ mod navsat {
     fixed_payload_len = 10,
     flags = "default_for_builder"
 )]
-struct CfgInf{
+struct CfgInf {
     protocol_id: u8,
     reserved: [u8; 3],
     #[ubx(map_type = CfgInfMask)]
@@ -1679,6 +1751,8 @@ define_recv_packets!(
         NavVelNed,
         NavTimeUTC,
         NavSat,
+        NavOdo,
+        CfgOdo,
         MgaAck,
         AlpSrv,
         AckAck,
