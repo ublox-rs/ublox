3e346b2f86292ef708ba19d31bb2e0d5c438f939
adrian-kong <adrian.k1234567890@gmail.com>
2022-08-22 12:30:45 +1000

    Add MgaGpsEPH

--- ublox/src/ubx_packets/packets.rs
+++ ublox/src/ubx_packets/packets.rs
@@ -2006,6 +2006,63 @@ struct NavVelECEF {
     s_acc: u32,
 }
 
+#[ubx_packet_recv]
+#[ubx(class = 0x13, id = 0x00, fixed_payload_len = 68)]
+struct MgaGpsEPH {
+    msg_type: u8,
+    version: u8,
+    sv_id: u8,
+    reserved1: u8,
+    fit_interval: u8,
+    ura_index: u8,
+    sv_health: u8,
+    #[ubx(map_type = f64, scale = 2e-31)]
+    tgd: i8,
+    iodc: u16,
+    #[ubx(map_type = f64, scale = 2e+4)]
+    toc: u16,
+    reserved2: u8,
+    #[ubx(map_type = f64, scale = 2e-55)]
+    af2: i8,
+    #[ubx(map_type = f64, scale = 2e-43)]
+    afl: i16,
+    #[ubx(map_type = f64, scale = 2e-31)]
+    af0: i32,
+    #[ubx(map_type = f64, scale = 2e-5)]
+    crs: i16,
+    #[ubx(map_type = f64, scale = 2e-43)]
+    delta_n: i16,
+    #[ubx(map_type = f64, scale = 2e-31)]
+    m0: i32,
+    #[ubx(map_type = f64, scale = 2e-29)]
+    cuc: i16,
+    #[ubx(map_type = f64, scale = 2e-29)]
+    cus: i16,
+    #[ubx(map_type = f64, scale = 2e-33)]
+    e: u32,
+    #[ubx(map_type = f64, scale = 2e-19)]
+    sqrt_a: u32,
+    #[ubx(map_type = f64, scale = 2e+4)]
+    toe: u16,
+    #[ubx(map_type = f64, scale = 2e-29)]
+    cic: i16,
+    #[ubx(map_type = f64, scale = 2e-31)]
+    omega0: i32,
+    #[ubx(map_type = f64, scale = 2e-29)]
+    cis: i16,
+    #[ubx(map_type = f64, scale = 2e-5)]
+    crc: i16,
+    #[ubx(map_type = f64, scale = 2e-31)]
+    i0: i32,
+    #[ubx(map_type = f64, scale = 2e-31)]
+    omega: i32,
+    #[ubx(map_type = f64, scale = 2e-43)]
+    omega_dot: i32,
+    #[ubx(map_type = f64, scale = 2e-43)]
+    idot: i16,
+    reserved3: [u8; 2],
+}
+
 define_recv_packets!(
     enum PacketRef {
         _ = UbxUnknownPacketRef,
@@ -2039,5 +2096,6 @@ define_recv_packets!(
         EsfMeas,
         NavClock,
         NavVelECEF,
+        MgaGpsEph,
     }
 );
