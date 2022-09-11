6d01ef10662c311be5ed6c25c50491d83e1f7f10
Aaron Holtzman <aaron@appliedmind.ca>
2022-09-11 12:15:15 -0500

    Add a few time related messages

--- ublox/src/ubx_packets/packets.rs
+++ ublox/src/ubx_packets/packets.rs
@@ -1146,6 +1146,94 @@ bitflags! {
     }
 }
 
+/// Time mode survey-in status
+#[ubx_packet_recv]
+#[ubx(class = 0x0d, id = 0x04, fixed_payload_len = 28)]
+struct TimSvin{
+    /// Passed survey-in minimum duration
+    /// Units: s
+    dur: u32,
+    /// Current survey-in mean position ECEF X coordinate
+    mean_x: i32,
+    /// Current survey-in mean position ECEF Y coordinate
+    mean_y: i32,
+    /// Current survey-in mean position ECEF Z coordinate
+    mean_z: i32,
+    /// Current survey-in mean position 3D variance
+    mean_v: i32,
+    /// Number of position observations used during survey-in
+    obs: u32,
+    /// Survey-in position validity flag, 1 = valid, otherwise 0
+    valid: u8,
+    /// Survey-in in progress flag, 1 = in-progress, otherwise 0
+    active: u8,
+    reserved: [u8; 2]
+}
+
+/// Leap second event information
+#[ubx_packet_recv]
+#[ubx(class = 0x01, id = 0x26, fixed_payload_len = 24)]
+struct NavTimeLs {
+    /// GPS time of week of the navigation epoch in ms.
+    i_tow: u32,
+    ///Message version (0x00 for this version)
+    version: u8,
+    reserved_1: [u8; 3],
+    /// Information source for the current number of leap seconds.
+    /// 0: Default (hardcoded in the firmware, can be outdated)
+    /// 1: Derived from time difference between GPS and GLONASS time
+    /// 2: GPS
+    /// 3: SBAS
+    /// 4: BeiDou
+    /// 5: Galileo
+    /// 6: Aided data 7: Configured 8: NavIC
+    /// 255: Unknown
+    src_of_curr_ls: u8,
+    /// Current number of leap seconds since start of GPS time (Jan 6, 1980). It reflects how much
+    /// GPS time is ahead of UTC time. Galileo number of leap seconds is the same as GPS. BeiDou
+    /// number of leap seconds is 14 less than GPS. GLONASS follows UTC time, so no leap seconds.
+    current_ls: i8,
+    /// Information source for the future leap second event.
+    /// 0: No source
+    /// 2: GPS
+    /// 3: SBAS
+    /// 4: BeiDou
+    /// 5: Galileo
+    /// 6: GLONASS 7: NavIC
+    src_of_ls_change: u8,
+    /// Future leap second change if one is scheduled. +1 = positive leap second, -1 = negative
+    /// leap second, 0 = no future leap second event scheduled or no information available.
+    ls_change: i8,
+    /// Number of seconds until the next leap second event, or from the last leap second event if
+    /// no future event scheduled. If > 0 event is in the future, = 0 event is now, < 0 event is in
+    /// the past. Valid only if validTimeToLsEvent = 1.
+    time_to_ls_event: i32,
+    /// GPS week number (WN) of the next leap second event or the last one if no future event
+    /// scheduled. Valid only if validTimeToLsEvent = 1.
+    date_of_ls_gps_wn: u16,
+    /// GPS day of week number (DN) for the next leap second event or the last one if no future
+    /// event scheduled. Valid only if validTimeToLsEvent = 1. (GPS and Galileo DN: from 1 = Sun to
+    /// 7 = Sat. BeiDou DN: from 0 = Sun to 6 = Sat.)
+    date_of_ls_gps_dn: u16,
+    reserved_2: [u8; 3],
+    /// Validity flags see `NavTimeLsFlags`
+    #[ubx(map_type = NavTimeLsFlags)]
+    valid: u8,
+}
+
+#[ubx_extend_bitflags]
+#[ubx(from, rest_reserved)]
+bitflags! {
+    /// Fix status flags for `NavTimeLsFlags`
+    pub struct NavTimeLsFlags: u8 {
+        /// 1 = Valid current number of leap seconds value.
+        const VALID_CURR_LS = 1;
+        /// Valid time to next leap second event or from the last leap second event if no future
+        /// event scheduled.
+        const VALID_TIME_TO_LS_EVENT = 2;
+    }
+}
+
 /// Time MODE3 Config Frame (32.10.37.1)
 /// only available on `timing` receivers
 #[ubx_packet_recv_send]
@@ -2812,6 +2900,7 @@ define_recv_packets!(
         NavSolution,
         NavVelNed,
         NavTimeUTC,
+        NavTimeLs,
         NavSat,
         NavEoe,
         NavOdo,
@@ -2842,6 +2931,7 @@ define_recv_packets!(
         MonVer,
         MonGnss,
         MonHw,
-        RxmRtcm
+        RxmRtcm,
+        TimSvin,
     }
 );
