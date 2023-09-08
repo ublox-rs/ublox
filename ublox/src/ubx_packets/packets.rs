019f6aa7935a6f7f45b403f5081d95f2ddf692af
Andrei Gherghescu <8067229+andrei-ng@users.noreply.github.com>
2023-09-08 11:40:33 +0200

    add HNR-ATT and HNR-INS messages
    Signed-off-by: Andrei Gherghescu <8067229+andrei-ng@users.noreply.github.com>

--- ublox/src/ubx_packets/packets.rs
+++ ublox/src/ubx_packets/packets.rs
@@ -3183,7 +3183,7 @@ impl<'a> core::iter::Iterator for EsfRawDataIter<'a> {
 #[ubx_packet_recv]
 #[ubx(class = 0x10, id = 0x15, fixed_payload_len = 36)]
 struct EsfIns {
-    // #[ubx(map_type = EsfInsBitFlags)]
+    #[ubx(map_type = EsfInsBitFlags)]
     bit_field: u32,
     reserved: [u8; 4],
     i_tow: u32,
@@ -3212,6 +3212,68 @@ struct EsfIns {
 bitflags! {
     #[derive(Debug)]
     pub struct EsfInsBitFlags: u32 {
+        const VERSION = 1;
+        const X_ANG_RATE_VALID = 0x100;
+        const Y_ANG_RATE_VALID = 0x200;
+        const Z_ANG_RATE_VALID = 0x400;
+        const X_ACCEL_VALID = 0x800;
+        const Y_ACCEL_VALID = 0x1000;
+        const Z_ACCEL_VALID = 0x2000;
+    }
+}
+
+#[ubx_packet_recv]
+#[ubx(class = 0x28, id = 0x01, fixed_payload_len = 32)]
+struct HnrAtt {
+    itow: u32,
+    version: u8,
+    reserved1: [u8; 3],
+    #[ubx(map_type = f64, scale = 1e-5, alias = vehicle_roll)]
+    roll: i32,
+    #[ubx(map_type = f64, scale = 1e-5, alias = vehicle_pitch)]
+    pitch: i32,
+    #[ubx(map_type = f64, scale = 1e-5, alias = vehicle_heading)]
+    heading: i32,
+    #[ubx(map_type = f64, scale = 1e-5, alias = vehicle_roll_accuracy)]
+    acc_roll: u32,
+    #[ubx(map_type = f64, scale = 1e-5, alias = vehicle_pitch_accuracy)]
+    acc_pitch: u32,
+    #[ubx(map_type = f64, scale = 1e-5, alias = vehicle_heading_accuracy)]
+    acc_heading: u32,
+}
+
+#[ubx_packet_recv]
+#[ubx(class = 0x28, id = 0x02, fixed_payload_len = 36)]
+pub struct HnrIns {
+    #[ubx(map_type = HnrInsBitFlags)]
+    bit_field: u32,
+    reserved: [u8; 4],
+    i_tow: u32,
+
+    #[ubx(map_type = f64, scale = 1e-3, alias = x_angular_rate)]
+    x_ang_rate: i32,
+
+    #[ubx(map_type = f64, scale = 1e-3, alias = y_angular_rate)]
+    y_ang_rate: i32,
+
+    #[ubx(map_type = f64, scale = 1e-3, alias = z_angular_rate)]
+    z_ang_rate: i32,
+
+    #[ubx(map_type = f64, scale = 1e-2, alias = x_acceleration)]
+    x_accel: i32,
+
+    #[ubx(map_type = f64, scale = 1e-2, alias = y_acceleration)]
+    y_accel: i32,
+
+    #[ubx(map_type = f64, scale = 1e-2, alias = z_acceleration)]
+    z_accel: i32,
+}
+
+#[ubx_extend_bitflags]
+#[ubx(from, rest_reserved)]
+bitflags! {
+    #[derive(Debug)]
+    pub struct HnrInsBitFlags: u32 {
         const VERSION = 1;
         const X_ANG_RATE_VALID = 0x100;
         const Y_ANG_RATE_VALID = 0x200;
@@ -3571,6 +3633,8 @@ define_recv_packets!(
         RxmRtcm,
         EsfMeas,
         EsfIns,
+        HnrAtt,
+        HnrIns,
         HnrPvt,
         NavAtt,
         NavClock,
