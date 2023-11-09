9568b41682cafd80fe27110b720dfea7b885185a
Andrei Gherghescu <8067229+andrei-ng@users.noreply.github.com>
2023-11-09 11:37:19 +0100

    refactor: small update in doc & README
    Signed-off-by: Andrei Gherghescu <8067229+andrei-ng@users.noreply.github.com>

--- ublox/src/lib.rs
+++ ublox/src/lib.rs
@@ -2,7 +2,8 @@
 //!
 //! This project aims to build a pure-rust I/O library for ublox GPS devices, specifically using the UBX protocol.
 //!
-//! An example of using this library to talk to a device can be seen in the ublox_cli subfolder of this project.
+//! An example of using this library to talk to a device can be seen in the examples/basic_cli subfolder of this project.
+//! More examples are available in the examples subfolder.
 //!
 //! Constructing Packets
 //! ====================
