7162d99294fcf0fee7651061b24d2cb4c92a2653
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-05-09 17:43:42 +0200

    GPS: add comments
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- ublox/src/ubx_packets/packets/rxm_sfrbx/gps.rs
+++ ublox/src/ubx_packets/packets/rxm_sfrbx/gps.rs
@@ -1,8 +1,11 @@
 // Preamble is masked out by UBX
-const GPS_PREAMBLE_MASK: u32 = 0xff000000;
-const GPS_PREAMBLE_SHIFT: u32 = 24;
+const GPS_TLM_PREAMBLE_MASK: u32 = 0xff000000;
+const GPS_TLM_PREAMBLE_SHIFT: u32 = 24;
 const GPS_TLM_MESSAGE_MASK: u32 = 0x00fffc00;
 const GPS_TLM_MESSAGE_SHIFT: u32 = 8;
+const GPS_TLM_INTEGRITY_BIT_MASK: u32 = 0x00000200;
+const GPS_TLM_RESERVED_BIT_MASK: u32 = 0x00000100;
+const GPS_TLM_PARITY_MASK: u32 = 0x000000ff;
 
 const GPS_HOW_TOW_MASK: u32 = 0xffff8000;
 const GPS_HOW_TOW_SHIFT: u32 = 13;
@@ -15,21 +18,37 @@ const GPS_HOW_FRAME_ID_SHIFT: u32 = 10;
 #[derive(Debug, Clone)]
 /// [GpsTelemetryWord]
 pub struct GpsTelemetryWord {
+    /// Preamble data bits. Should be constant. Ublox seems to mask some of them ?
     pub preamble: u8,
+
+    /// TLM Message
     pub tlm_message: u16,
-    // pub integrity: u8,
-    // pub reserved: u8,
-    // pub parity: u8,
+
+    /// Integrity bit is asserted means the conveying signal is provided
+    /// with an enhanced level of integrity assurance.
+    pub integrity: bool,
+
+    /// Reserved bit
+    pub reserved: bool,
+
+    /// Parity bits
+    pub parity: u8,
 }
 
 impl GpsTelemetryWord {
     pub fn decode(dword: u32) -> Option<Self> {
-        let preamble = ((dword & GPS_PREAMBLE_MASK) >> GPS_PREAMBLE_SHIFT) as u8;
+        let preamble = ((dword & GPS_TLM_PREAMBLE_MASK) >> GPS_TLM_PREAMBLE_SHIFT) as u8;
         let tlm_message = ((dword & GPS_TLM_MESSAGE_MASK) >> GPS_TLM_MESSAGE_SHIFT) as u16;
+        let integrity = (dword & GPS_TLM_INTEGRITY_BIT_MASK) > 0;
+        let reserved = (dword & GPS_TLM_INTEGRITY_BIT_MASK) > 0;
+        let parity = (dword & GPS_TLM_PARITY_MASK) as u8;
 
         Some(Self {
             preamble,
             tlm_message,
+            integrity,
+            reserved,
+            parity,
         })
     }
 }
@@ -38,11 +57,20 @@ impl GpsTelemetryWord {
 #[derive(Debug, Clone)]
 /// [GpsHowWord]
 pub struct GpsHowWord {
+    /// Elapsed seconds in GPST week
     pub tow: u32,
+
+    /// When alert is asserted, the SV URA may be worse than indicated in subframe 1
+    /// and user shall use this SV at their own risk.
     pub alert: bool,
+
+    /// A-S mode is ON in that SV
     pub anti_spoofing: bool,
+
+    /// Following Frame ID (to decoding following data words)
     pub frame_id: u8,
 }
+
 impl GpsHowWord {
     pub fn decode(dword: u32) -> Option<Self> {
         let tow = (dword & GPS_HOW_TOW_MASK) >> GPS_HOW_TOW_SHIFT;
